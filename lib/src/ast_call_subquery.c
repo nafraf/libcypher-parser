d76f7c9a38fda4922a8e5f7121cdf88f1e6b0ea9
raz-mon <razmonsonego2@gmail.com>
2023-06-20 07:58:53 +0300

    fix leak

--- lib/src/ast_call_subquery.c
+++ lib/src/ast_call_subquery.c
@@ -92,10 +92,9 @@ void cypher_ast_call_subquery_replace_query
     REQUIRE_TYPE(astnode, CYPHER_AST_CALL_SUBQUERY, NULL);
     struct call_subquery *node =
             container_of(astnode, struct call_subquery, _astnode);
-    // if (node->query != NULL)
-    // {
-    //     cypher_astnode_free(node->query);
-    // }
+    if (node->query != NULL) {
+        cypher_astnode_free(node->query);
+    }
     astnode->children[0] = query;
     node->query = query;
 }
